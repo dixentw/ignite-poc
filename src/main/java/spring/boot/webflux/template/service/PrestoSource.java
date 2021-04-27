f20dbe098f45df2272cff39078654aa4cd08ee2e
dixentw <dixentw@gmail.com>
2021-04-27 16:53:49 +0900

    add

--- src/main/java/spring/boot/webflux/template/service/PrestoSource.java
+++ src/main/java/spring/boot/webflux/template/service/PrestoSource.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Service;
 import spring.boot.webflux.template.model.User;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -18,7 +16,7 @@ public class PrestoSource {
     private final String tableName = "users";
 
 
-    public void clickHouseBatchInsert(List<User> users) {
+    public void clickHouseBatchInsert(List<Map<String, Object>> users) {
         try {
             long startTime = System.currentTimeMillis();
             String clickhouseIPAddr = "10.0.15.17:9000,10.0.15.16:9000,10.0.15.15:9000,10.0.15.14:9000";
@@ -28,13 +26,31 @@ public class PrestoSource {
             Connection conn = clickHouseDataSource.getConnection();
             conn.setAutoCommit(false);
             PreparedStatement pstmt = conn.prepareStatement(
-                    "INSERT INTO push_user_2 (id, Last_Name, " +
-                            "First_Name, Email, Phone_Number)" +
-                            " VALUES(?,?,?,?,?)");
+                    "INSERT INTO push_user_3_all (id, device_token, code, push_token, creation_timestamp, update_timestamp, general_configuration, server_configuration" +
+                            "profile, last_delivery_timestamp, last_survey_timestamp, edition, push_time, sign)" +
+                            " VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
             int cnt = 0;
-            for (User u: users) {
-                pstmt.setLong(1, u.id);
-                pstmt.setString(2, u.deviceToken);
+            for (Map<String, Object> u: users) {
+                String edition = "";
+                if(((Long) u.get("id")) % 2 ==0) {
+                    edition = "ja_JP";
+                } else {
+                    edition = "en_US";
+                }
+                pstmt.setLong(1, (Long) u.get("id"));
+                pstmt.setString(2, (String) u.get("device_token"));
+                pstmt.setString(3, (String) u.get("code"));
+                pstmt.setString(4, (String) u.get("push_token"));
+                pstmt.setLong(5, (Long) u.get("create_timestamp"));
+                pstmt.setLong(6, (Long) u.get("update_timestamp"));
+                pstmt.setString(7, (String) u.get("general_configuration"));
+                pstmt.setString(8, (String) u.get("server_configuration"));
+                pstmt.setString(9, (String) u.get("profile"));
+                pstmt.setLong(10, (Long) u.get("create_timestamp"));
+                pstmt.setLong(11, (Long) u.get("create_timestamp"));
+                pstmt.setString(12, edition);
+                pstmt.setString(13, "10800");
+                pstmt.setInt(14, 10);
                 pstmt.addBatch();
                 cnt++;
             }
@@ -52,7 +68,6 @@ public class PrestoSource {
         }
     }
 
-
     public void loadUsersFromPresto(int offset, int length) {
         log.info("parameters, off: {}, length {}", offset, length);
         offset += 100;
@@ -63,33 +78,26 @@ public class PrestoSource {
             properties.setProperty("user", "nobody");
             Connection connection = DriverManager.getConnection(url, properties);
             Statement statement = connection.createStatement();
-            String sql = String.format("select id, device_token, push_token, general_configuration from %s.%s where id >%d and id<=%d and dt='2021-04-26'", dbName, tableName, offset, offset+length);
+            String sql = String.format("select id, code, create_timestamp, update_timestamp device_token, push_token, general_configuration, server_configuration, profile from %s.%s where id >%d and id<=%d and dt='2021-04-26'", dbName, tableName, offset, offset+length);
             log.info("try to query as =====   {}", sql);
             ResultSet rs = statement.executeQuery(sql);
             int cnt = 0;
-            List<User> users = new ArrayList<>();
+            List<Map<String, Object>> users = new ArrayList<>();
             while (rs.next()) {
-                User u = new User();
-                u.id = rs.getLong("id");
-                u.setting = rs.getString("general_configuration");
-                u.deviceToken = rs.getString("device_token");
-                u.pushToken = rs.getString("push_token");
-                if (u.id % 2 == 0) {
-                    u.edition = "ja_JP";
-                } else {
-                    u.edition = "en_US";
-                }
-                if (u.id % 3 == 0) {
-                    u.morning = 10800;
-                } else if (u.id % 3 == 1) {
-                    u.morning = 97000;
-                } else if (u.id % 3 == 2) {
-                    u.morning = 12600;
-                }
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", rs.getLong("id"));
+                map.put("code", rs.getString("code"));
+                map.put("create_timestamp", rs.getLong("create_timestamp"));
+                map.put("update_timestamp", rs.getLong("update_timestamp"));
+                map.put("device_token", rs.getString("device_token"));
+                map.put("push_token", rs.getString("push_token"));
+                map.put("general_configuration", rs.getString("general_configuration"));
+                map.put("server_configuration", rs.getString("server_configuration"));
+                map.put("profile", rs.getString("profile"));
                 cnt++;
-                users.add(u);
+                users.add(map);
                 if (cnt % 10000 ==0) {
-                    log.info("sample user: {}", u);
+                    log.info("sample user: {}", map);
                     clickHouseBatchInsert(users);
                     users.clear();
                 }
